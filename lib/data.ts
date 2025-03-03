8a8ea232632d0ccf0ae69feeee85c3306e9be22d
Zack <qq26283871@gmail.com>
2025-03-03 10:52:22 +0800

    03-新增主頁輪播

--- lib/data.ts
+++ lib/data.ts
@@ -33,6 +33,30 @@ const data = {
         href: '/page/help',
       },
     ],
+
+    carousels: [
+      {
+        title: '最熱門鞋款特賣',
+        buttonCaption: '立即選購',
+        image: '/images/banner3.jpg',
+        url: '/search?category=Shoes',
+        isPublished: true,
+      },
+      {
+        title: 'T 恤暢銷冠軍',
+        buttonCaption: '立即選購',
+        image: '/images/banner1.jpg',
+        url: '/search?category=T-Shirts',
+        isPublished: true,
+      },
+      {
+        title: '經典腕表搶購',
+        buttonCaption: '查看更多',
+        image: '/images/banner2.jpg',
+        url: '/search?category=Wrist Watches',
+        isPublished: true,
+      },
+    ],
   }
   
   export default data
